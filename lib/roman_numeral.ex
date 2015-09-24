4d1879f8623bfae58e1245a8134af0572a938109
Sonny Scroggin <scrogson@gmail.com>
2015-09-24 09:50:50 -0500

    Occasionally we can add roman numerals

--- lib/roman_numeral.ex
+++ lib/roman_numeral.ex
@@ -1,2 +1,20 @@
 defmodule RomanNumeral do
+  def roman_sum(x, y) do
+    xy = expandio(x) <> expandio(y)
+    reductio(xy)
+  end
+
+  defp reductio("IIII"), do: "IV"
+  defp reductio("IIIII"), do: "V"
+  defp reductio("IIIIIIIII"), do: "IX"
+  defp reductio("IIIIIIIIII"), do: "X"
+  defp reductio(letters), do: letters
+
+  # convert to I's
+  defp expandio("IV"), do: "IIII"
+  defp expandio("V"), do: "IIIII"
+  defp expandio(letters) do
+    letters
+  end
 end
+
--- test/roman_numeral_test.exs
+++ test/roman_numeral_test.exs
@@ -1,7 +1,37 @@
 defmodule RomanNumeralTest do
   use ExUnit.Case
 
+  import RomanNumeral
+
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "I + I = II" do
+    assert roman_sum("I", "I") == "II"
+  end
+
+  test "I + II = III" do
+    assert roman_sum("I", "II") == "III"
+  end
+
+  test "II + III = V" do
+    assert roman_sum("II", "III") == "V"
+  end
+
+  test "V + V = X" do
+    assert roman_sum("V", "V") == "X"
+  end
+
+  test "I + III = IV" do
+    assert roman_sum("I", "III") == "IV"
+  end
+
+  test "IV + V = IX" do
+    assert roman_sum("IV", "V") == "IX"
+  end
+
+  test "V + IV = IX" do
+    assert roman_sum("V", "IV") == "IX"
   end
 end
